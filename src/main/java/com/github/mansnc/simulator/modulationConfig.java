4abae11abe4592167f32d440db52162109badab9
Matt Naslcheraghi <mansour.nch@gmail.com>
2023-10-25 10:45:57 -0400

    Modulation config is added

---
+++ src/main/java/com/github/mansnc/simulator/modulationConfig.java
@@ -0,0 +1,16 @@
+package com.github.mansnc.simulator;
+
+public class modulationConfig {
+
+    
+        public int modulationScheme = 6; 
+        // 2: QPSK
+        // 4: QAM-16 
+        // 6: QAM-64 
+        // 8: QAM-256
+        // pi2: pi/2-BPSK
+        
+        //public bit2symMapping = 
+
+    
+}
